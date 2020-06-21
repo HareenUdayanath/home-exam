c6cd33f2b334599dad4d67849c7ffff38357d81e
HareenUdayanath <hareenudayanath@gmail.com>
2020-06-21 22:20:09 +0530

    Adding service tests

--- src/app/services/helpers/result-calculation-helper/result-calculation-helper.service.spec.ts
+++ src/app/services/helpers/result-calculation-helper/result-calculation-helper.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { Level } from '../../../shared/models/constant';
 
 import { ResultCalculationHelperService } from './result-calculation-helper.service';
 
@@ -13,4 +14,13 @@ describe('ResultCalculationHelperService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('check skill levels', () => {
+      expect(service.getLevel(10)).toBe(Level.WEAK);
+      expect(service.getLevel(65)).toBe(Level.OK);
+      expect(service.getLevel(80)).toBe(Level.GOOD);
+      expect(service.getLevel(95)).toBe(Level.EXCELLENT);
+    }
+  );
+
 });
